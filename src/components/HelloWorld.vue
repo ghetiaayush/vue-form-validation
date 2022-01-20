b10a60193e2b8a957cc992fe4d76dfbf8e7e039d
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2022-01-20 10:38:10 +0530

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+<FormValidation></FormValidation>  
+  </div>
+</template>
+
+<script>
+import FormValidation from '@/components/FormValidation.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    FormValidation
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
